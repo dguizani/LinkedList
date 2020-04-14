aa306425b54bb3234dda121d576a07049cc35eb3
DaviGuizaniCarvalho <daviguizani@gmail.com>
2020-04-13 22:13:23 -0300

    Update linkedList.c
    Adicionada e implementada a função newLinkedList

--- code/libs/linkedList/linkedList.c
+++ code/libs/linkedList/linkedList.c
@@ -1,5 +1,18 @@
+#include <stdlib.h>
+#include "../bool/bool.c"
+
 typedef struct linkedList
 {
     int value;
     struct linkedList *next;
 } EList;
+
+/*
+ * Função para iniciar uma nova lista
+ * 
+ * return NULL: sempre
+*/
+EList *newLinkedList()
+{
+    return NULL;
+}
