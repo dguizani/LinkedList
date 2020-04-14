b73d93d5bca165cda5f991a8457679e4e1b83307
EmployingDGC <daviguizani@gmail.com>
2020-04-14 18:39:34 -0300

    Update linkedList.c
    Implementada a função removeFirstList
    
    Nome elementList do parâmetro das funções alterado para element_list

--- code/libs/linkedList/linkedList.c
+++ code/libs/linkedList/linkedList.c
@@ -20,14 +20,14 @@ EList *newLinkedList()
 /*
  * Função para testar se a posição da lista está vazia
  * 
- * param elementList: referencia para um ponteiro de um elemento da lista
+ * param element_list: referencia para um ponteiro de um elemento da lista
  * 
  * return true: se está vazia
  * return false: se não está vazia
 */
-Bool listIsEmpty(EList **elementList)
+Bool listIsEmpty(EList **element_list)
 {
-    if (*elementList == NULL)
+    if (*element_list == NULL)
         return true;
     
     return false;
@@ -36,13 +36,13 @@ Bool listIsEmpty(EList **elementList)
 /*
  * Função para adicionar um elemento no inicio da lista
  * 
- * param elementList: referencia para um ponteiro de um elemento da lista
+ * param element_list: referencia para um ponteiro de um elemento da lista
  * param value: valor a ser adicionado na lista
  * 
  * return true: se conseguiu adicionar
  * return false: se não conseguiu adicionar
 */
-Bool addFirstList(EList **elementList, int value)
+Bool addFirstList(EList **element_list, int value)
 {
     EList *new = (EList *) malloc(sizeof(EList));
 
@@ -51,17 +51,17 @@ Bool addFirstList(EList **elementList, int value)
     
     new -> value = value;
 
-    if (listIsEmpty(elementList))
+    if (listIsEmpty(element_list))
     {
         new -> next = NULL;
-        *elementList = new;
+        *element_list = new;
 
         return true;
     }
 
-    new -> next = *elementList;
+    new -> next = *element_list;
 
-    *elementList = new;
+    *element_list = new;
 
     return true;
 }
@@ -69,16 +69,16 @@ Bool addFirstList(EList **elementList, int value)
 /*
  * Função para adicionar um elemento no fim da lista
  * 
- * param elementList: referencia para um ponteiro de um elemento da lista
+ * param element_list: referencia para um ponteiro de um elemento da lista
  * param value: valor a ser adicionado na lista
  * 
  * return true: se conseguiu adicionar
  * return false: se não conseguiu adicionar
 */
-Bool addLastList(EList **elementList, int value)
+Bool addLastList(EList **element_list, int value)
 {
-    if (listIsEmpty(elementList))
-        return addFirstList(elementList, value);
+    if (listIsEmpty(element_list))
+        return addFirstList(element_list, value);
     
     EList *new = (EList *) malloc(sizeof(EList));
 
@@ -88,7 +88,7 @@ Bool addLastList(EList **elementList, int value)
     new -> value = value;
     new -> next = NULL;
 
-    EList *aux = *elementList;
+    EList *aux = *element_list;
     for (aux; aux -> next != NULL; aux = aux -> next);
     
     aux -> next = new;
@@ -99,24 +99,24 @@ Bool addLastList(EList **elementList, int value)
 /*
  * Função para adicionar um elemento na posição desejada da lista
  * 
- * param elementList: referencia para um ponteiro de um elemento da lista
+ * param element_list: referencia para um ponteiro de um elemento da lista
  * param value: valor a ser adicionado na lista
  * param posX: posição a ser adicionado o elemento na lista
  * 
  * return true: se conseguiu adicionar
  * return false: se não conseguiu adicionar
 */
-Bool addIndexList(EList **elementList, int value, unsigned int posX)
+Bool addIndexList(EList **element_list, int value, unsigned int posX)
 {
-    if (listIsEmpty(elementList) || posX == 0)
-        return addFirstList(elementList, value);
+    if (listIsEmpty(element_list) || posX == 0)
+        return addFirstList(element_list, value);
     
     EList *new = (EList *) malloc(sizeof(EList));
 
     if (listIsEmpty(&new))
         return false;
 
-    EList *current = *elementList;
+    EList *current = *element_list;
     for (current; current -> next != NULL || posX == 1; current = current -> next)
         posX -= 1;
 
@@ -131,12 +131,21 @@ Bool addIndexList(EList **elementList, int value, unsigned int posX)
 /*
  * Função para remover o primeiro elemento da lista
  * 
- * param elementList: referencia para um ponteiro de um elemento da lista
+ * param element_list: referencia para um ponteiro de um elemento da lista
  * 
  * return true: se conseguiu remover
  * return false: se não conseguiu remover
 */
-Bool removeFirstList(EList **elementList)
+Bool removeFirstList(EList **element_list)
 {
+    if (listIsEmpty(element_list))
+        return false;
+    
+    EList *aux = *element_list;
 
+    *element_list = aux -> next;
+
+    free(aux);
+
+    return true;
 }
